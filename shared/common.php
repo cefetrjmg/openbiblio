c8857a3ca8676bdcb1d98617ebc57aa185779571
Cristian Madeira de Souza Pereira <cristianpereira@tic.ufrj.br>
2019-12-02 15:33:49 -0300

    Fixed the problem of timezone.

--- shared/common.php
+++ shared/common.php
@@ -1,4 +1,5 @@
 <?php
+  date_default_timezone_set(getenv("TIMEZONE"));
 /* This file is part of a copyrighted work; it is distributed with NO WARRANTY.
  * See the file COPYRIGHT.html for more details.
  */
